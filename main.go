62d47e3a0329711e5e8ff0978812ff34fdce8d53
livia <livia.c.silva6@aluno.senai.br>
2025-04-10 09:38:01 -0300

    if else

--- main.go
+++ main.go
@@ -3,16 +3,29 @@ package main
 import "fmt"
 
 func main() {
-    var numeros [5]int
-    var soma int
+    age := 45
+	fmt.Println(age <= 50 )
+	fmt.Println(age >= 50)
+	fmt.Println(age == 50)
+	fmt.Println(age != 50)
 
-    fmt.Println("Digite 5 números:")
-
-    for i := 0; i < 5; i++ {
-        fmt.Scan(&numeros[i])
-        soma += numeros[i]
-    }
-
-    fmt.Println("Soma:", soma)
+	if age < 30 {
+		fmt.Println("menor que 30 anos")
+	} else if age < 40 {
+		fmt.Println("menor que 40 anos")
+	} else { 
+		fmt.Println("não é mesnor que 40 anos")
+	}
+	names := []string{"beatriz", "livia", "joana", "mel"}
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continue apos posição", index, "e valor", value)
+			continue
+		}
+		if index > 2 { 
+			fmt.Println("sair apos" , index)
+		}
+		fmt.Println("valor: ", value)
+	}
 }
  
